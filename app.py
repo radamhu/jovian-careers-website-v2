25f0417fa695652001b0f636462c6c0c1e3b52ce
Ádám Roland <roland.adam@hotmail.com>
2023-02-21 17:22:02 +0000

    added connection to DB

--- app.py
+++ app.py
@@ -1,42 +1,20 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'TS, 100.000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Bengaluru, India',
-    'salary': 'TS, 100.000'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote',
-    'salary': ''
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Fransico, USa',
-    'salary': '$120.000'
-  }
-]
-
+  
 @app.route("/")
 def hello_jovian():
-    return render_template('home.html', 
-                           jobs=JOBS,
+  jobs = load_jobs_from_db()
+  # jobs = jobs : az első jobs vairable amit a home.html-ben használunk
+  # a második jobs az pedig jobs = load_...
+  return render_template('home.html', 
+                          jobs=jobs,
                           company_name='Jovian')
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
